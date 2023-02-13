37ba4249e44f5c4300846b906f010b59bf356e30
Rennan <reiony.dev@gmail.com>
2023-02-13 13:35:46 -0300

    feat: customers middleware

---
+++ src/middlewares/customers.middleware.js
@@ -0,0 +1,26 @@
+import { customersSchema } from "../schemas/customers.schema.js";
+import { connection } from "../database/db.js";
+
+export async function validateCustomerSchema(req, res, next) {
+  const customer = req.body;
+
+  const { error } = customersSchema.validate(customer, { abortEarly: false });
+  if (error) {
+    const errorDetails = error.details.map((e) => e.message);
+    return res.status(400).send({ errorDetails });
+  }
+
+  const cpfAlreadyExists = await connection.query(
+    "SELECT * FROM customers WHERE cpf=$1",
+    [customer.cpf]
+  );
+
+  if (cpfAlreadyExists.rowCount !== 0) {
+    return res
+      .status(409)
+      .send({ message: "Conflict: CPF is already in use." });
+  }
+
+  res.locals.customer = customer;
+  next();
+}
